c11389474ebfb98c842678d1a48eed92ebea5b3a
Olga Kopylova <Olga_kopylova9@mail.ru>
2026-01-31 14:27:06 +0500

    первый коммит задания 1

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 // Создаю  переменные
-let title = 'Мой проект';
-let screens = 'Простые, Сложные, Интерактивные';
-let screenPrice = 100;
-let rollback = 10;
-let fullPrice = 500;
-let adaptive = true;
+// let title = 'Мой проект';
+// let screens = 'Простые, Сложные, Интерактивные';
+// let screenPrice = 100;
+// let rollback = 10;
+// let fullPrice = 500;
+// let adaptive = true;
 
-// Вывожу значения в консоль для проверки
-console.log('Название:', title);
-console.log('Типы экранов:', screens);
-console.log('Цена за экран:', screenPrice);
-console.log('Процент отката:', rollback);
-console.log('Полная стоимость:', fullPrice);
-console.log('Адаптивная верстка:', adaptive);
+// // Вывожу значения в консоль для проверки
+// console.log('Название:', title);
+// console.log('Типы экранов:', screens);
+// console.log('Цена за экран:', screenPrice);
+// console.log('Процент отката:', rollback);
+// console.log('Полная стоимость:', fullPrice);
+// console.log('Адаптивная верстка:', adaptive);
 
-console.log('Hello, Git!');
+// console.log('Hello, Git!');
+
+const a = 1;
+const b = "вторая переменная"
