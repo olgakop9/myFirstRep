2e155e517714bd36fe95b577579aa79fe73512b3
Olga Kopylova <Olga_kopylova9@mail.ru>
2026-01-31 14:11:02 +0500

    Мой первый коммит

---
+++ script.js
@@ -0,0 +1,15 @@
+// Создаю  переменные
+let title = 'Мой проект';
+let screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 100;
+let rollback = 10;
+let fullPrice = 500;
+let adaptive = true;
+
+// Вывожу значения в консоль для проверки
+console.log('Название:', title);
+console.log('Типы экранов:', screens);
+console.log('Цена за экран:', screenPrice);
+console.log('Процент отката:', rollback);
+console.log('Полная стоимость:', fullPrice);
+console.log('Адаптивная верстка:', adaptive);
