4683a28a4cffde8ef7b8871d6f31a6074e5a589f
Olga Kopylova <Olga_kopylova9@mail.ru>
2026-01-31 15:01:11 +0500

    Типы данных

---
+++ index.js
@@ -0,0 +1,13 @@
+let title = "Проект по разработке приложения"
+let screens = "Простые, Сложные, Интерактивные"
+let screenPrice = 1500  //цена за экран
+let rollback = 15  // от 1 до 100
+let fullPrice = 100000 // сколько хотите заработать
+let adaptive = true //булевое значение
+
+
+console.log(title);
+console.log(screenPrice);
+console.log(rollback);
+console.log(fullPrice);
+console.log(adaptive);
