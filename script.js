d6da10a39dfcf5f010d5c79f7da45283beeee76d
Olga Kopylova <Olga_kopylova9@mail.ru>
2026-02-06 19:46:51 +0500

    leccon5

---
+++ script.js
@@ -0,0 +1,107 @@
+//Приведение типа данных
+'use strict'
+let rollback = 10
+let allServicePrices
+let fullPrice
+let servicePercentPrice
+
+
+let title 
+let screens 
+let screenPrice 
+let adaptive 
+
+let service1
+let service2 
+
+const isNumber = function (num) {
+        return !isNaN(parseFloat(num)) && isFinite(num)
+}
+
+
+const asking = function() {
+        title = prompt("Как называется ваш проект?", "Калькулятор верстки")
+        screens = prompt('Какие типы экранов нужно разработать?', "Простые  и сложные")
+        screenPrice = prompt('Сколько будет стоить данная работа?')
+
+        // while(!isNumber(screenPrice)) {
+        //   screenPrice = prompt('Сколько будет стоить данная работа?')
+        // }
+
+         do {
+        screenPrice = prompt('Сколько будет стоить данная работа?')
+    } while(!isNumber(screenPrice))
+    
+        adaptive = confirm('Нужен ли адаптив на сайте?')
+
+}
+
+const getAllServicePrices = function () {
+        let sum = 0;
+        
+        for(let i = 0; i < 2; i++) {
+
+                if (i === 0) {
+                service1 = prompt('Какой дополнительный тип услуги нужен?')
+                } else if (i === 1) {
+                service2 =  prompt('Какой дополнительный тип услуги нужен?')
+        }
+
+
+                sum += +prompt("Сколько будет стоить?")
+
+                while(!isNumber(allServicePrices)) {
+                allServicePrices = prompt('Сколько будет стоить данная работа?')
+         }
+        } 
+        return sum
+
+        // return servicePrice1 + servicePrice2
+}
+const showTypeOf = function (variable) {
+        console.log(variable, typeof variable);
+}
+
+const getFullPrice = function () {
+        return screenPrice + allServicePrices
+}
+
+const getServicePercentPrice = function () {
+        return fullPrice - (fullPrice * (rollback / 100))
+}
+
+const getTitle = function () {
+        return title.trim()[0].toUpperCase() + title.trim().substr(1).toLowerCase()
+}
+
+const getRollbackMessage = function (price) {
+        if (price >= 30000) {
+                return "Даем скидку 10%"
+        }else if (price >= 15000 && price < 30000) {
+                return "Даем скидку 5%"
+        } else if (price >= 0 && price < 15000) {
+                return "кидка не предусмотрена"
+        } else {
+                return "что то пошло не так"
+        }
+}
+
+asking()
+allServicePrices = getAllServicePrices()
+fullPrice = getFullPrice()
+servicePercentPrice = getServicePercentPrice()
+title = getTitle
+
+showTypeOf(title)
+showTypeOf(screenPrice)
+showTypeOf(adaptive)
+
+console.log("allServicePrices", allServicePrices);
+
+console.log(getRollbackMessage(fullPrice));
+console.log(typeof title);
+console.log(typeof screenPrice);
+console.log(typeof adaptive);
+
+console.log("Стоимость верстки экранов " + screenPrice + "юани" + " и стоимость разработки сайта" + fullPrice + "юани")
+ 
